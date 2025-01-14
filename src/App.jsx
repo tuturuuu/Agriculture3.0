17142e8656822884b588191dd10d173e3000a9f1
tuturuuu <viettpm26@gmail.com>
2025-01-14 17:18:45 +0700

    added material ui

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,19 @@ function App() {
 
       {/* image canvas? here */}
       <div className="position-relative">
-        <img src="src\assets\png\background.png" alt="" width="100%" />
+        <img src="src\assets\png\background.png" alt="" width="100%" height="575px" style={{objectFit: 'cover'}}/>
 
-        <h1 style={{top: '35%', left: '50%', position: 'absolute', transform: 'translate(-50%, -50%)'}} >Algriculture Blockchain Trading Platform </h1>
-
-        <button className="position-absolute top-50 start-50 translate-middle btn btn-darkw" type="button">Hi</button>
+        <span style={{top: '50%', left: '50%', position: 'absolute', transform: 'translate(-50%, -50%)', textAlign: 'center', fontSize: '3rem'}} className="fw-semibold" >
+          Algriculture Blockchain Trading Platform
+            <button style={{width:'55%', marginTop: '3%', borderRadius:'30px'}} className="btn btn-lg btn-dark fs-4 fw-semibold 
+            " type="button">Get started</button>
+        </span>
       </div>
 
+      {/* Popular */}
+      <section className="p-5">
+        <h1 className="fs-3 fw-bold">Popular 🔥</h1>
+      </section>
 
     </>
   )
